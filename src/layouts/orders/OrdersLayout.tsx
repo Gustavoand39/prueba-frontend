ae984d6ae8970bbcf867c9eba02ab7db3bf0672e
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-18 17:35:02 -0600

    feat: refactor enrutamiento de órdenes y eliminar componente OrderRoutes

--- src/layouts/orders/OrdersLayout.tsx
+++ src/layouts/orders/OrdersLayout.tsx
@@ -1,18 +1,17 @@
 import { Suspense } from "react";
 import LoadingState from "@/components/states/LoadingState";
 import OrdersHeader from "@/layouts/orders/header/OrdersHeader";
+import { Outlet } from "react-router-dom";
 
-interface OrdersLayoutProps {
-  children: React.ReactNode;
-}
-
-const OrdersLayout = ({ children }: OrdersLayoutProps) => {
+const OrdersLayout = () => {
   return (
     <div className="flex h-screen w-full flex-col">
       <OrdersHeader />
 
       <main className="flex-1">
-        <Suspense fallback={<LoadingState />}>{children}</Suspense>
+        <Suspense fallback={<LoadingState />}>
+          <Outlet />
+        </Suspense>
       </main>
     </div>
   );
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,17 +1,40 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import OrderRoutes from "./OrderRoutes";
+// Lazy-loaded views
+const UpcomingOrdersView = lazy(
+  () => import("@/views/orders/UpcomingOrdersView"),
+);
+const DetailsOrderView = lazy(() => import("@/views/orders/DetailsOrderView"));
+
+// Components
+import LoadingState from "@/components/states/LoadingState";
+import OrdersLayout from "@/layouts/orders/OrdersLayout";
 
 const AppRouter = () => {
   return (
     <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Navigate to="/orders/upcoming" />} />
+      <Suspense fallback={<LoadingState fullScreen />}>
+        <Routes>
+          {/* Redirect root to orders/upcoming */}
+          <Route path="/" element={<Navigate to="/orders/upcoming" />} />
 
-        <Route path="/orders/*" element={<OrderRoutes />} />
+          {/* Orders routes */}
+          <Route path="/orders/*" element={<OrdersLayout />}>
+            <Route>
+              <Route path="upcoming" element={<UpcomingOrdersView />} />
+            </Route>
 
-        <Route path="*" element={<Navigate to="/orders/upcoming" />} />
-      </Routes>
+            <Route path=":id" element={<DetailsOrderView />} />
+
+            {/* Redirect invalid routes to orders/upcoming */}
+            <Route path="*" element={<Navigate to="/orders/upcoming" />} />
+          </Route>
+
+          {/* Redirect any invalid route */}
+          <Route path="*" element={<Navigate to="/orders/upcoming" />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
