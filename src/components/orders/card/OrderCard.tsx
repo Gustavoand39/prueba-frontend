96ed3eb8c0be0e523b309b42047d50f78ac0b911
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-18 11:40:58 -0600

    feat: simplificar el acceso a propiedades del pedido en el componente OrderCard

--- src/components/orders/card/OrderCard.tsx
+++ src/components/orders/card/OrderCard.tsx
@@ -14,25 +14,29 @@ interface OrderCardProps {
  * @returns {JSX.Element} Order Card Component
  */
 const OrderCard = ({ order }: OrderCardProps): JSX.Element => {
+  const { order_number, type, status_string, destinations, status_class } =
+    order;
+
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-xl">
         <span className="text-neutral-400">Order</span>{" "}
-        <span>#{order.order_number}</span>
+        <span>#{order_number}</span>
       </h2>
 
       <section className="rounded-3xl border-2 border-neutral-600">
         {/* Header */}
         <OrderCardHeader
-          type={order.type}
-          status_string={order.status_string}
+          type={type}
+          statusString={status_string}
+          statusClass={status_class}
         />
 
         {/* Divider */}
         <hr className="border border-neutral-600" />
 
         {/* Content */}
-        <OrderCardContent destinations={order.destinations} />
+        <OrderCardContent destinations={destinations} />
 
         {/* Footer */}
         <OrderCardFooter />
