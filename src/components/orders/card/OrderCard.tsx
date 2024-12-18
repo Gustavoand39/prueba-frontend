3a0e429e71d1fd01469c284ed20354cb10e6c449
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-17 19:22:55 -0600

    refactor: mejorar el componente OrderCard
    - Añade la documentación y mejor el tamaño del título

--- src/components/orders/card/OrderCard.tsx
+++ src/components/orders/card/OrderCard.tsx
@@ -7,24 +7,34 @@ interface OrderCardProps {
   order: Order;
 }
 
-const OrderCard = ({ order }: OrderCardProps) => {
+/**
+ * Order Card Component
+ * @param {OrderCardProps} props - Order Card Props
+ * @property {Order} props.order - Order
+ * @returns {JSX.Element} Order Card Component
+ */
+const OrderCard = ({ order }: OrderCardProps): JSX.Element => {
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-lg">
+      <h2 className="text-xl">
         <span className="text-neutral-400">Order</span>{" "}
         <span>#{order.order_number}</span>
       </h2>
 
       <section className="rounded-3xl border-2 border-neutral-600">
+        {/* Header */}
         <OrderCardHeader
           type={order.type}
           status_string={order.status_string}
         />
 
+        {/* Divider */}
         <hr className="border border-neutral-600" />
 
-        <OrderCardContent />
+        {/* Content */}
+        <OrderCardContent destinations={order.destinations} />
 
+        {/* Footer */}
         <OrderCardFooter />
       </section>
     </div>
