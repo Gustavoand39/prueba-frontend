a31f4fb5a9e0c272788f051252b8191fb9e3953e
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-17 19:25:06 -0600

    feat: agregar contenido dinámico al componente OrderCardContent
    - Muestra los destinos de recolección y de entrega

--- src/components/orders/card/OrderCardContent.tsx
+++ src/components/orders/card/OrderCardContent.tsx
@@ -1,9 +1,68 @@
-const OrderCardContent = () => {
-  return (
-    <div className="flex flex-col gap-6 px-6 py-8">
-      <div>Pickup</div>
+import { Box, MapPin } from "lucide-react";
+import { Destination } from "@/types/models.types";
 
-      <div>Dropoff</div>
+interface OrderCardContentProps {
+  destinations: Destination[];
+}
+
+/**
+ * Shows the content of the order card.
+ * Contains the pickup and dropoff destinations.
+ * @param {OrderCardContentProps} props - Order Card Content Props
+ * @property {Destination[]} props.destinations - Destinations
+ * @returns {JSX.Element} Order Card Content Component
+ */
+const OrderCardContent = ({
+  destinations,
+}: OrderCardContentProps): JSX.Element => {
+  return (
+    <div className="flex flex-col gap-4 px-6 py-8">
+      {destinations.map(({ address, end_date, nickname, start_date }) => {
+        // Get the city from the address
+        const addressParts = address.split(",");
+        const city = addressParts[addressParts.length - 1].trim();
+
+        return (
+          <div key={start_date} className="flex items-center justify-between">
+            <div className="flex flex-1 items-center gap-4">
+              {/* Icon */}
+              <div className="flex-shrink-0">
+                {nickname === "Recolección" ? (
+                  <Box size={24} />
+                ) : (
+                  <MapPin size={24} />
+                )}
+              </div>
+
+              {/* Details */}
+              <div className="flex flex-col">
+                <span className="text-xs font-semibold text-neutral-500">
+                  {nickname === "Recolección" ? "PICKUP" : "DROPOFF"}
+                </span>
+                <span className="text-base font-semibold text-neutral-100">
+                  {city ?? "N/A"}
+                </span>
+                <span className="w-40 overflow-hidden text-ellipsis whitespace-nowrap text-xs text-neutral-400 sm:w-64 md:text-sm xl:w-96">
+                  {address}
+                </span>
+              </div>
+            </div>
+
+            {/* Date & Time */}
+            <div className="flex flex-col text-right">
+              <span className="text-xs text-neutral-400">
+                {new Date(start_date).toLocaleDateString()}
+              </span>
+              <span className="text-sm font-semibold text-neutral-100">
+                {new Date(end_date).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
