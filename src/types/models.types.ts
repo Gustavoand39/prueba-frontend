de3008e8128ab43a620cc5caa103a6e5a071e3ed
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-17 18:01:56 -0600

    feat: agregar interfaces para modelos de destino y orden en typescript

---
+++ src/types/models.types.ts
@@ -0,0 +1,38 @@
+/**
+ * Destination object
+ */
+export interface Destination {
+  address: string;
+  start_date: number;
+  end_date: number;
+  nickname: string;
+  show_navigation: boolean;
+}
+
+/**
+ * Order object
+ */
+export interface Order {
+  _id: string;
+  status: number;
+  order_number: string;
+  manager: string;
+  driver: string;
+  version: string;
+  type: string;
+  destinations: Destination[];
+  start_date: number;
+  end_date: number;
+  is_today: boolean;
+  status_string: string;
+  status_class: string;
+  driver_thumbnail: null;
+}
+
+/**
+ * Response from the API
+ */
+export interface Result {
+  status: number;
+  result: Order[];
+}
