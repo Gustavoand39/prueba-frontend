3ab6e4a36fe30ba629529471f983c4da82cc0e99
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-18 15:48:25 -0600

    feat: agregar mapeo de estado de destino a colores y traducción en DestinationCardContent

--- src/components/orders/details/destination-card/DestinationsCardContent.tsx
+++ src/components/orders/details/destination-card/DestinationsCardContent.tsx
@@ -5,11 +5,34 @@ interface DestintationCardContentProps {
   destinations: DestinationDetailed[];
 }
 
+/**
+ * Mapping of destination status to color
+ * @type {Record<string, string>}
+ */
+const DestinationStatusColor: Record<string, string> = {
+  Aceptado: "bg-blue-300",
+  "En espera": "bg-neutral-300",
+};
+
+/**
+ * Mapping of destination status from Spanish to English
+ * @type {Record<string, string>}
+ */
+const DestinationStatus: Record<string, string> = {
+  Aceptado: "Accepted",
+  "En espera": "On Hold",
+};
+
+/**
+ * Shows the content of the destination card.
+ * @param {DestintationCardContentProps} props
+ * @returns {JSX.Element}
+ */
 const DestinationCardContent = ({
   destinations,
 }: DestintationCardContentProps): JSX.Element => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="relative flex flex-col gap-6">
       {destinations.map((destination, index) => {
         const isPickup = index === 0; // First destination is pickup
         const city = destination.address.split(",").pop()?.trim() || "N/A";
@@ -23,7 +46,7 @@ const DestinationCardContent = ({
             <div>{isPickup ? <Box size={24} /> : <MapPin size={24} />}</div>
 
             {/* Details */}
-            <div className="flex w-60 flex-col sm:w-full">
+            <div className="flex w-60 flex-col space-y-0.5 sm:w-full">
               <span className="text-xs font-semibold text-neutral-500">
                 {isPickup ? "PICKUP" : "DROPOFF"}
               </span>
@@ -31,13 +54,19 @@ const DestinationCardContent = ({
               <span className="w-full truncate text-xs text-neutral-400">
                 {destination.address}
               </span>
-              <span className="text-xs text-neutral-400">
-                {destination.status_string}
-              </span>
+              <div className="flex items-center gap-2 text-xs text-neutral-400">
+                <div
+                  className={`size-3 rounded-full ${DestinationStatusColor[destination.status_string]}`}
+                />
+                <span>{DestinationStatus[destination.status_string]}</span>
+              </div>
             </div>
           </div>
         );
       })}
+
+      {/* Line to separate destinations */}
+      <div className="absolute left-[12px] top-[60px] h-[65px] w-0.5 bg-gradient-to-b from-white to-neutral-950" />
     </div>
   );
 };
