ea3a77c5c1c7ff371ad39b1b550e0a6bfdd46aad
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-18 16:38:58 -0600

    feat: agregar componente DestinationDetails y su integración en DestinationsCardContent

--- src/components/orders/details/destination-card/DestinationsCardContent.tsx
+++ src/components/orders/details/destination-card/DestinationsCardContent.tsx
@@ -1,27 +1,15 @@
-import { DestinationDetailed } from "@/types/models.types";
+import { useState } from "react";
 import { Box, MapPin } from "lucide-react";
+import DestinationDetails from "@/components/orders/details/destination-card/DestinationDetails";
+import {
+  DestinationStatus,
+  DestinationStatusColor,
+} from "@/constants/DestinationStatus";
+import { DestinationDetailed } from "@/types/models.types";
 
 interface DestintationCardContentProps {
   destinations: DestinationDetailed[];
 }
-
-/**
- * Mapping of destination status to color
- * @type {Record<string, string>}
- */
-const DestinationStatusColor: Record<string, string> = {
-  Aceptado: "bg-blue-300",
-  "En espera": "bg-neutral-300",
-};
-
-/**
- * Mapping of destination status from Spanish to English
- * @type {Record<string, string>}
- */
-const DestinationStatus: Record<string, string> = {
-  Aceptado: "Accepted",
-  "En espera": "On Hold",
-};
 
 /**
  * Shows the content of the destination card.
@@ -31,43 +19,75 @@ const DestinationStatus: Record<string, string> = {
 const DestinationCardContent = ({
   destinations,
 }: DestintationCardContentProps): JSX.Element => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedDestination, setSelectedDestination] =
+    useState<DestinationDetailed | null>(null);
+  const [isPickup, setIsPickup] = useState(false);
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
+
+  const handleDestinationSelect = (
+    destination: DestinationDetailed,
+    isPickup: boolean,
+  ) => {
+    setSelectedDestination(destination);
+    setIsPickup(isPickup);
+    setIsDrawerOpen(true);
+  };
+
   return (
-    <div className="relative flex flex-col gap-6">
-      {destinations.map((destination, index) => {
-        const isPickup = index === 0; // First destination is pickup
-        const city = destination.address.split(",").pop()?.trim() || "N/A";
+    <>
+      <div className="relative flex flex-col gap-6">
+        {destinations.map((destination, index) => {
+          const isPickup = index === 0;
+          const city = destination.address.split(",").pop()?.trim() || "N/A";
 
-        return (
-          <div
-            key={destination.address}
-            className="flex flex-1 items-center gap-4"
-          >
-            {/* Icon */}
-            <div>{isPickup ? <Box size={24} /> : <MapPin size={24} />}</div>
+          return (
+            <div
+              key={destination.address}
+              className="flex flex-1 cursor-pointer items-center gap-4"
+              role="button"
+              tabIndex={0}
+              onClick={() => handleDestinationSelect(destination, isPickup)}
+            >
+              {/* Icon */}
+              <div>{isPickup ? <Box size={24} /> : <MapPin size={24} />}</div>
 
-            {/* Details */}
-            <div className="flex w-60 flex-col space-y-0.5 sm:w-full">
-              <span className="text-xs font-semibold text-neutral-500">
-                {isPickup ? "PICKUP" : "DROPOFF"}
-              </span>
-              <span className="text-base font-semibold">{city}</span>
-              <span className="w-full truncate text-xs text-neutral-400">
-                {destination.address}
-              </span>
-              <div className="flex items-center gap-2 text-xs text-neutral-400">
-                <div
-                  className={`size-3 rounded-full ${DestinationStatusColor[destination.status_string]}`}
-                />
-                <span>{DestinationStatus[destination.status_string]}</span>
+              {/* Details */}
+              <div className="flex w-60 flex-col space-y-0.5 p-1 sm:w-full">
+                <span className="text-xs font-semibold text-neutral-500">
+                  {isPickup ? "PICKUP" : "DROPOFF"}
+                </span>
+                <span className="text-base font-semibold">{city}</span>
+                <span className="w-full truncate text-xs text-neutral-400">
+                  {destination.address}
+                </span>
+                <div className="flex items-center gap-2 text-xs text-neutral-400">
+                  <div
+                    className={`size-3 rounded-full ${DestinationStatusColor[destination.status_string]}`}
+                  />
+                  <span>{DestinationStatus[destination.status_string]}</span>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
 
-      {/* Line to separate destinations */}
-      <div className="absolute left-[12px] top-[60px] h-[65px] w-0.5 bg-gradient-to-b from-white to-neutral-950" />
-    </div>
+        {/* Line to separate destinations */}
+        <div className="absolute left-[12px] top-[65px] h-[65px] w-0.5 bg-gradient-to-b from-white via-neutral-600 to-neutral-950" />
+      </div>
+
+      {selectedDestination && (
+        <DestinationDetails
+          isOpen={isDrawerOpen}
+          openChange={handleDrawerClose}
+          destination={selectedDestination}
+          isPickup={isPickup}
+        />
+      )}
+    </>
   );
 };
 
