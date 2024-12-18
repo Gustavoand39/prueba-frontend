868a8bf6500ad5daf3d8035ef71675e6757d823a
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-12-18 11:41:11 -0600

    feat: mejorar el componente OrdersHeader con título dinámico y ajustes de estilo

--- src/layouts/orders/header/OrdersHeader.tsx
+++ src/layouts/orders/header/OrdersHeader.tsx
@@ -1,23 +1,41 @@
 import { Bell, ChevronLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useLocation } from "react-router-dom";
 
-const OrdersHeader = () => {
+/**
+ * Title View
+ * Contains the title for the Orders Layout
+ */
+const TitleView: Record<string, string> = {
+  "/orders/upcoming": "Upcoming Orders",
+};
+
+/**
+ * Orders Header Component
+ * Contains the header for the Orders Layout
+ * @returns {JSX.Element} Orders Header Component
+ */
+const OrdersHeader = (): JSX.Element => {
+  const location = useLocation();
+
   return (
     <header>
       <div className="container mx-auto flex items-center justify-between px-4 py-6">
         <div>
           <Button variant="ghost" size="icon">
-            <ChevronLeft size={28} />
+            <ChevronLeft size={24} />
           </Button>
         </div>
 
         <div>
-          <h1 className="text-xl font-semibold">Upcoming Orders</h1>
+          <h1 className="text-xl font-semibold">
+            {TitleView[location.pathname] ?? "Orders"}
+          </h1>
         </div>
 
         <div>
           <Button variant="ghost" size="icon">
-            <Bell className="text-yellow-300" size={28} />
+            <Bell className="text-custom-primary" size={24} />
           </Button>
         </div>
       </div>
